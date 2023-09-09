ba497823a7ae334a9b2b5a785561772bd9d8adba
Vedic Laptop <vedic20052005@gmail.com>
2023-09-09 12:39:52 +0530

    Trying to make it work

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import './index.css';
-import myPage from '../public/pages/main';
+import myPage from './main';
 
 function App() {
   const location = useLocation();
 
   return (
     <Routes>
-      <Route path='/projects/naithani' element={<myPage/>} />
-      <Route path='/projects/naithani/*' element={<Navigate to="/projects/naithani" state={{ from: location.pathname }} />} />
+      <Route path='/projects/naithani' element={()=>{<p>working?</p>;}} /> {/* Here it is used */}
+      <Route
+        path='/projects/naithani/*'
+        element={
+          <Navigate to="/projects/naithani" state={{ from: location.pathname }} />
+        }
+      />
     </Routes>
   );
 }
+
 
 const root = createRoot(document.getElementById('root'));
 root.render(
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Banner from "./components/Banner";
+import DescriptionAndContact from "./components/Description";
+import ProductList from "./components/ProductCardList";
+function App() {
+return (
+  <>
+  <Banner />
+  <DescriptionAndContact />
+  <ProductList />
+</>);
+}
+
+export default App;
